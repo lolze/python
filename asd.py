283f5f1b98a3e1496c91e60919891fce1e088fdd
lolzy <dimich956@gmail.com>
2021-11-03 03:35:57 +0500

    newas

--- asd.py
+++ asd.py
@@ -2,11 +2,11 @@ a = int (input())
 b = int (input())
 c = int (input())
 d = int (input())
-for g in range ( c, d+1 ):
-    print( '\t' +str(g), end='' )
-print( end='\n' )
-for i in range ( a, b+1 ):
+for g in range (c, d+1):
+    print('\t' +str(g), end='')
+print(end='\n')
+for i in range (a, b+1):
     print(str(i) + '\t', end='')
-    for j in range ( c, d+1 ):
+    for j in range (c, d+1):
         print(str(i*j) ,end='\t')
     print(end='\n')
