22299961fa95f3766d0cf289fcdc8591536ffe97
lolzy <dimich956@gmail.com>
2021-11-03 03:31:44 +0500

    commit

--- asd.py
+++ asd.py
@@ -6,7 +6,7 @@ for g in range ( c, d+1 ):
     print( '\t' +str(g), end='' )
 print( end='\n' )
 for i in range ( a, b+1 ):
-    print( str(i) + '\t', end='' )
+    print(str(i) + '\t', end='')
     for j in range ( c, d+1 ):
-        print( str(i*j) ,end='\t' )
+        print(str(i*j) ,end='\t')
     print(end='\n')
